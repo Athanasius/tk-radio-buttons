2d4b0959c1b6761c2df21aa4cb10968158edf237
Athanasius <github@miggy.org>
2020-10-07 16:56:01 +0100

    Add Python 3.9.0 specific BAT file

---
+++ python-390-local-edmc.bat
@@ -0,0 +1,5 @@
+@Set PYTHONPATH='C:\\Program Files (x86)\\Python39-32\\python39.zip', 'C:\\Users\\Athan\\AppData\\Local\\Programs\\Python\\Python39-32\\Lib', 'C:\\Users\\Athan\\AppData\\Local\\Programs\\Python\\Python39-32\\DLLs', 'C:\\Program Files (x86)\\Python39-32\\Lib', 'C:\\Program Files (x86)\\Python39-32\\DLLs', 'C:\\Users\\Athan\\Documents\\Devel\\tk-radio-buttons', 'C:\\Users\\Athan\\AppData\\Roaming\\Python\\Python39\\site-packages', 'C:\\Program Files (x86)\\Python39-32', 'C:\\Program Files (x86)\\Python39-32\\lib\\site-packages'
+
+set
+
+python-390.exe edmc.py
