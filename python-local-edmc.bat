3043ad37591aabeb6e53c2645f79b11b7428a1bd
Athanasius <github@miggy.org>
2020-10-07 15:14:03 +0100

    BAT file to run local python.exe with PYTHONPATH set
    This is so we can change the manifest on a local copy without messing
    with the installed copy.

---
+++ python-local-edmc.bat
@@ -0,0 +1,3 @@
+Set PYTHONPATH='C:\\Program Files (x86)\\Python37-32\\python37.zip', 'C:\\Users\\Athan\\AppData\\Local\\Programs\\Python\\Python37-32\\Lib', 'C:\\Users\\Athan\\AppData\\Local\\Programs\\Python\\Python37-32\\DLLs', 'C:\\Program Files (x86)\\Python37-32\\Lib', 'C:\\Program Files (x86)\\Python37-32\\DLLs', 'C:\\Users\\Athan\\Documents\\Devel\\tk-radio-buttons', 'C:\\Users\\Athan\\AppData\\Roaming\\Python\\Python37\\site-packages', 'C:\\Program Files (x86)\\Python37-32', 'C:\\Program Files (x86)\\Python37-32\\lib\\site-packages'
+
+python.exe edmc.py
